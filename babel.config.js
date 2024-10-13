37829e67ffd6f39873d016148d377a54af04f4f8
Yaroslav Borys <borrline@gmail.com>
2024-10-13 19:30:38 +0300

    changed babel config

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,22 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./"],
+          extensions: [".ts", ".tsx", ".js", ".json", ".png"],
+          alias: {
+            "@app": "./app",
+            "@assets": "./assets",
+            "@components": "./components",
+            "@hooks": "./hooks",
+            "@constants": "./constants",
+          },
+        },
+      ],
+    ],
   };
 };
