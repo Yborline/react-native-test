118936317af1242237593452440f82d001770ed6
Yaroslav Borys <borrline@gmail.com>
2024-10-13 17:20:47 +0300

    added module

---
+++ expo-env.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="expo/types" />
+
+// NOTE: This file should not be edited and should be in your git ignore
+
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.svg";
