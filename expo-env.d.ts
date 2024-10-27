25ddb72b15b6250eef5bcfc3fff04dffc4600ac9
Yaroslav Borys <borrline@gmail.com>
2024-10-27 16:47:59 +0300

    fixed png

--- expo-env.d.ts
+++ expo-env.d.ts
@@ -1,7 +1,6 @@
 /// <reference types="expo/types" />
 
 // NOTE: This file should not be edited and should be in your git ignore
-
 declare module "*.png";
 declare module "*.jpg";
 declare module "*.jpeg";
