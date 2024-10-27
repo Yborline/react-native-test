064c48b0a38a788d3f1617280f61b437a97956eb
Yaroslav Borys <borrline@gmail.com>
2024-10-27 21:03:24 +0300

    changed styles and logic and added photo  in the register

--- app/(tabs)/register.tsx
+++ app/(tabs)/register.tsx
@@ -17,7 +17,12 @@ import { TextInput } from "react-native";
 import { Link } from "expo-router";
 import Input from "@components/input/Input";
 import InputPassword from "@components/input/InputPassword/InputPassword";
+import * as ImagePicker from "expo-image-picker";
+// import SvgComponent from "@assets/svg/add";
+import SvgAdd from "@assets/svg/add";
+
 const Register = () => {
+  const [image, setImage] = useState<string | null>(null);
   const [name, setName] = useState<string>("");
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
@@ -25,21 +30,59 @@ const Register = () => {
   const handleLogin = () => {
     Alert.alert("Credentials", `${name} + ${password}`);
   };
+  const handleTouchMove = (event) => {
+    console.log("sssss");
+  };
 
+  const pickImage = async () => {
+    // Запит дозволу на доступ до галереї
+    if (Platform.OS !== "web") {
+      const { status } =
+        await ImagePicker.requestMediaLibraryPermissionsAsync();
+      if (status !== "granted") {
+        alert("Потрібен доступ до галереї!");
+        return;
+      }
+    }
+
+    // Вибір фото
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+  console.log(image);
   return (
     <ImageBackground source={startBg} style={styles.image}>
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <View style={styles.conteiner}>
+        <View style={styles.container}>
           <KeyboardAvoidingView
             keyboardVerticalOffset={-200}
             style={styles.boxAboiding}
             behavior={"padding"}
           >
             <View style={styles.boxForm}>
+              <View onTouchStart={pickImage} style={styles.boxPhoto}>
+                <ImageBackground
+                  style={{ width: "100%", height: "100%" }}
+                  source={image ? { uri: image } : undefined}
+                >
+                  <SvgAdd fill="gray" />
+                  {/* <SvgComponent style={styles.add} fill="gray" /> */}
+                </ImageBackground>
+              </View>
               <View style={styles.boxInput}>
                 <Text style={styles.textTitle}>Реєстрація</Text>
                 <Input
-                  type="username"
+                  type="nickname"
                   placeholder="Ваш логін"
                   onChange={setName}
                 />
@@ -67,7 +110,7 @@ const Register = () => {
 export default Register;
 
 const styles = StyleSheet.create({
-  conteiner: {
+  container: {
     flex: 1,
     width: "100%",
     height: "100%",
@@ -107,6 +150,21 @@ const styles = StyleSheet.create({
     paddingLeft: 16,
     borderTopLeftRadius: 25,
     borderTopRightRadius: 25,
+  },
+  boxPhoto: {
+    width: 120,
+    height: 120,
+    position: "absolute",
+    top: -60,
+    alignSelf: "center",
+    backgroundColor: "rgba(246, 246, 246, 1)",
+    borderRadius: 16,
+    overflow: "hidden",
+  },
+  add: {
+    position: "absolute",
+    bottom: 14,
+    right: -12,
   },
   changeMargin: {
     marginBottom: Platform.OS === "ios" ? 60 : 30,
