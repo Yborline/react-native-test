cdd83933d73c3ffb70330cb3a2994427fc277f21
Yaroslav Borys <borrline@gmail.com>
2024-10-28 21:55:30 +0300

    changed path

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/explore` | `/(tabs)/login` | `/(tabs)/register` | `/..\components\Collapsible` | `/..\components\HelloWave` | `/..\components\input\InputPassword\InputPassword` | `/..\components\navigation\TabBarIcon` | `/_sitemap` | `/explore` | `/login` | `/register`;
+      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/explore` | `/(tabs)/login` | `/(tabs)/register` | `/_sitemap` | `/explore` | `/login` | `/register`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
