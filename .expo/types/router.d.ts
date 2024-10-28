4b2692128e1ca3dca8494f714e2f0467d7e4e83a
Yaroslav Borys <borrline@gmail.com>
2024-10-28 21:43:23 +0300

    added more tabs

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/explore` | `/(tabs)/profile` | `/_sitemap` | `/explore` | `/profile`;
+      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/explore` | `/(tabs)/login` | `/(tabs)/register` | `/_sitemap` | `/explore` | `/login` | `/register`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
